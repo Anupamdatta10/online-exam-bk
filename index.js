44514eca7cb0143f5ed62692384778e89698d162
Lucefer256 <sm30122000@gmail.com>
2022-12-05 16:27:18 +0530

    added delete

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use('/api/v1',mainRouter.publicRouter)
 
 
-app.get('/api/v1',)
+//app.get('/api/v1',)
 app.listen(PORT, () => {
 	console.log("Server up and running on port: %d", PORT);
 });
